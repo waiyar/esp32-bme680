d2e82873d0bb30c3f4b488f4bb9fb96d1a2b5f35
Wai Yar <waiyar.aung@meds-tech.com>
2019-06-07 17:47:01 +0800

    first commit

---
+++ main/i2c_example_main.c
@@ -0,0 +1,275 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+#include "bme680.h"
+#include "unistd.h"
+
+#include <inttypes.h> // for PRIi8
+
+#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
+SemaphoreHandle_t print_mux = NULL;
+
+struct bme680_dev gas_sensor;
+int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+void user_delay_ms(uint32_t period);
+void get_sensor_readings();
+
+ static const char* TAG = "i2c_bme680";
+
+void bme680_sensor_init()
+{
+
+    uint8_t set_required_settings;
+    int8_t rslt = BME680_OK;
+
+    gas_sensor.dev_id = BME680_I2C_ADDR_SECONDARY;
+	gas_sensor.intf = BME680_I2C_INTF;
+	gas_sensor.read = user_i2c_read;
+	gas_sensor.write = user_i2c_write;
+	gas_sensor.delay_ms = user_delay_ms;
+	/* amb_temp can be set to 25 prior to configuring the gas sensor
+	* or by performing a few temperature readings without operating the gas sensor.
+	*/
+	gas_sensor.amb_temp = 25;
+
+	rslt = bme680_init(&gas_sensor);
+	printf("\nInitialized with code: %" PRIi8 , rslt);
+
+    /* Set the temperature, pressure and humidity settings */
+	gas_sensor.tph_sett.os_hum = BME680_OS_2X;
+	gas_sensor.tph_sett.os_pres = BME680_OS_4X;
+	gas_sensor.tph_sett.os_temp = BME680_OS_8X;
+	gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+
+	/* Set the remaining gas sensor settings and link the heating profile */
+	gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
+	/* Create a ramp heat waveform in 3 steps */
+	gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
+	gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
+
+	/* Select the power mode */
+	/* Must be set before writing the sensor configuration */
+	gas_sensor.power_mode = BME680_FORCED_MODE;
+
+	/* Set the required sensor settings needed */
+	set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL
+		| BME680_GAS_SENSOR_SEL;
+    printf("\nBefore settings with code: %" PRIi8 , rslt);
+
+	/* Set the desired sensor configuration */
+	rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
+    printf("\nSettings set with code: %" PRIi8 , rslt);
+
+    /* Set the power mode */
+    rslt = bme680_set_sensor_mode(&gas_sensor);
+    printf("\nPower mode set with code: %" PRIi8 , rslt);
+
+    get_sensor_readings();
+}
+
+void get_sensor_readings()
+{
+    int8_t rslt = BME680_OK;
+
+    /* Get the total measurement duration so as to sleep or wait till the
+     * measurement is complete */
+    uint16_t meas_period;
+    bme680_get_profile_dur(&meas_period, &gas_sensor);
+
+    struct bme680_field_data data;
+
+    while(1)
+    {
+        user_delay_ms(meas_period); /* Delay till the measurement is ready */
+
+        rslt = bme680_get_sensor_data(&data, &gas_sensor);
+
+        printf("T: %.2f degC, P: %.2f hPa, H %.2f %%rH ", data.temperature / 100.0f,
+            data.pressure / 100.0f, data.humidity / 1000.0f );
+        /* Avoid using measurements from an unstable heating setup */
+        if(data.status & BME680_GASM_VALID_MSK)
+            printf(", G: %d OHMS", data.gas_resistance);
+
+        printf("\r\n");
+
+        /* Trigger the next measurement if you would like to read data out continuously */
+        if (gas_sensor.power_mode == BME680_FORCED_MODE) {
+            rslt = bme680_set_sensor_mode(&gas_sensor);
+        }
+        sleep(2);
+    }
+}
+
+void user_delay_ms(uint32_t period)
+{
+    /*
+     * Return control or wait,
+     * for a period amount of milliseconds
+     */
+	sleep(period / 1000);
+}
+
+
+int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+
+    /*
+     * The parameter dev_id can be used as a variable to store the I2C address of the device
+     */
+
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Stop       | -                   |
+     * | Start      | -                   |
+     * | Read       | (reg_data[0])       |
+     * | Read       | (....)              |
+     * | Read       | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (dev_id << 1) | WRITE_BIT, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+	i2c_master_stop(cmd);
+	esp_err_t ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+    vTaskDelay(30 / portTICK_RATE_MS);
+
+    cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (dev_id << 1) | READ_BIT, ACK_CHECK_EN);
+	i2c_master_read(cmd, reg_data, len, ACK_VAL);
+	i2c_master_read_byte(cmd, reg_data + len - 1, NACK_VAL);
+	i2c_master_stop(cmd);
+	ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+	i2c_cmd_link_delete(cmd);
+
+    switch (ret) {
+    	case ESP_ERR_INVALID_STATE:
+    		ESP_LOGE(TAG, "ESP_ERR_INVALID_STATE");
+    		break;
+    	case ESP_ERR_INVALID_ARG:
+    		ESP_LOGE(TAG, "ESP_ERR_INVALID_ARG");
+    		break;
+    	case ESP_FAIL:
+    		ESP_LOGE(TAG, "ESP_FAIL");
+    		break;
+    	case ESP_ERR_TIMEOUT:
+    		ESP_LOGE(TAG, "ESP_ERR_TIMEOUT");
+    		break;
+    	case ESP_OK:
+    		rslt = 0;
+    		break;
+    	default:
+    		ESP_LOGE(TAG, "UNKNOWN ERROR");
+    }
+    return rslt;
+}
+
+int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
+{
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+
+    /*
+     * The parameter dev_id can be used as a variable to store the I2C address of the device
+     */
+
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Write      | (reg_data[0])       |
+     * | Write      | (....)              |
+     * | Write      | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_id << 1) | WRITE_BIT, ACK_CHECK_EN);
+	i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    i2c_master_write(cmd, reg_data, len, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    switch (ret) {
+    	case ESP_ERR_INVALID_STATE:
+    		ESP_LOGE(TAG, "ESP_ERR_INVALID_STATE");
+    		break;
+    	case ESP_ERR_INVALID_ARG:
+    		ESP_LOGE(TAG, "ESP_ERR_INVALID_ARG");
+    		break;
+    	case ESP_FAIL:
+    		ESP_LOGE(TAG, "ESP_FAIL");
+    		break;
+    	case ESP_ERR_TIMEOUT:
+    		ESP_LOGE(TAG, "ESP_ERR_TIMEOUT");
+    		break;
+    	case ESP_OK:
+    		rslt = 0;
+    		break;
+    	default:
+    		ESP_LOGE(TAG, "UNKNOWN ERROR");
+    }
+    return rslt;
+}
+
+
+static esp_err_t i2c_master_init()
+{
+    int i2c_master_port = I2C_NUM_0;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(i2c_master_port, conf.mode,
+                              I2C_MASTER_RX_BUF_DISABLE,
+                              I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+void app_main()
+{
+	printf("Main started\n");
+    ESP_ERROR_CHECK(i2c_master_init());
+
+    bme680_sensor_init();
+}
